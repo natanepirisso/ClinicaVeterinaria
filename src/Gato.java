8424674e0299d51e2e4b7394bd5244b679b7e4a5
natanepirisso <natanepirisso@gmail.com>
2025-08-13 10:03:23 -0300

    perf: Implementação de getters e setters

--- src/Gato.java
+++ src/Gato.java
@@ -2,4 +2,9 @@ public class Gato extends Animal{
     public Gato(String nome, int idade, String race, double peso){
         super(nome,idade,race,peso);
     }
+
+    @Override
+    public String toString(){
+        return "Nome do Gato: "+ getNome() + " | Idade: "+ getIdade() + " | Peso do Gato: "+ getPeso()+"kg"+ " | Raça: "+ getRace();
+    }
 }
