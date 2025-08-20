34a35526e8b8155ef874f32ab041969d9f909974
natanepirisso <natanepirisso@gmail.com>
2025-08-20 19:21:58 -0300

    feat: acrescentei o atributo sexo e o get nos seus devidos lugares

--- src/Cachorro.java
+++ src/Cachorro.java
@@ -1,10 +1,12 @@
+import enums.Sexo;
+
 public class Cachorro extends Animal{
-    public Cachorro(String nome, int idade, String race, double peso){
-        super(nome,idade,race,peso);
+    public Cachorro(String nome, int idade, String race, double peso, Sexo sexo){
+        super(nome,idade,race,peso,sexo);
 
     }
     @Override
     public String toString(){
-        return "Nome do Cachorro: "+ getNome() + " | Idade: "+ getIdade() + " | Peso do Cachorro: "+ getPeso()+"kg"+ " | Raça: "+ getRace() + " | ID: "+ getId();
+        return "Nome do Cachorro: "+ getNome() + " | Idade: "+ getIdade() + " | Peso do Cachorro: "+ getPeso()+"kg"+ " | Raça: "+ getRace() + " | Sexo: "+ getSexo() + " | ID: "+ getId();
     }
 }
--- src/Gato.java
+++ src/Gato.java
@@ -1,10 +1,12 @@
+import enums.Sexo;
+
 public class Gato extends Animal{
-    public Gato(String nome, int idade, String race, double peso){
-        super(nome,idade,race,peso);
+    public Gato(String nome, int idade, String race, double peso, Sexo sexo){
+        super(nome,idade,race,peso,sexo);
     }
 
     @Override
     public String toString(){
-        return "Nome do Gato: "+ getNome() + " | Idade: "+ getIdade() + " | Peso do Gato: "+ getPeso()+"kg"+ " | Raça: "+ getRace() + " | ID: " + getId();
+        return "Nome do Gato: "+ getNome() + " | Idade: "+ getIdade() + " | Peso do Gato: "+ getPeso()+"kg"+ " | Raça: "+ getRace() + " | Sexo: "+ getSexo() + " | ID: " + getId();
     }
 }
