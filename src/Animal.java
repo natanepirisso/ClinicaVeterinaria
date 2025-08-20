592ebe5f4165d7e40c8dbdb871a5bc1084ad2252
natanepirisso <natanepirisso@gmail.com>
2025-08-20 19:20:34 -0300

    feat: adicionei o atributo sexo na classe e seus determinados getters e setters

--- src/Animal.java
+++ src/Animal.java
@@ -1,18 +1,22 @@
+import enums.Sexo;
+
 public abstract class Animal {
     private String nome;
     private int idade;
     private String race;
     private double peso;
+    private Sexo sexo;
     private final String id;
 
     private static int contador = 1;
 
-    protected Animal(String nome, int idade, String race, double peso){
+    protected Animal(String nome, int idade, String race, double peso, Sexo sexo){
         this.nome = nome;
         this.idade = idade;
         this.race = race;
         this.peso = peso;
         this.id = gerarId();
+        this.sexo = sexo;
     }
     private String gerarId(){
         return String.format("AN-%03d", contador++);
@@ -23,9 +27,11 @@ public abstract class Animal {
     public int getIdade() {return idade;}
     public String getRace() {return race;}
     public double getPeso() {return peso;}
+    public Sexo getSexo(){return sexo;}
     public void setNome(String nome){ this.nome = nome;}
     public void setIdade(int idade){this.idade = idade;}
     public void setRace(String race){this.race = race;}
     public void setPeso(double peso){this.peso = peso;}
+    public void setSexo(Sexo sexo){this.sexo = sexo;}
     public abstract String toString();
 }
